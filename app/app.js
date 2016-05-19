fdb45d25cc0a0e541c41aa4f8b072074516a67d6
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-18 21:18:58 -0500

    added nav buttons

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
-console.log("Hello");
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function($scope){
+  $scope.navItems = [{name: "All Items"}, {name: "New Items"}, {name: "Logout"}];
+})
+
+
+app.controller("TodoCtrl", function($scope){
+  $scope.welcome = "Hello";
+})
+
+
 
 // We are using $-scoped notation, which is deprecated, but it's still found in 90% of the code
