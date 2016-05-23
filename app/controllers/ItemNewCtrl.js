0ba427c9de164b8f647bed807435d3ce3bafd8f2
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-23 18:25:25 -0500

    Replaced array with firebase The internal array was converted to a json file to upload to firebase.

--- app/controllers/ItemNewCtrl.js
+++ app/controllers/ItemNewCtrl.js
@@ -3,38 +3,7 @@
 app.controller("ItemNewCtrl", function($scope){
   $scope.newTask = {};
 
-  $scope.items = [
-    {
-      id: 0,
-      task: "mow the lawn",
-      isCompleted: true,
-      dueDate: "12/5/17",
-      assignedTo: "Greg",
-      location: "Zoe's House",
-      urgency: "Low",
-      dependencies: "sunshine, clippers, hat, water, headphones"
-    },
-    {
-      id: 1,
-      task: "Grade Quizzes",
-      isCompleted: true,
-      dueDate: "12/5/15",
-      assignedTo: "Joe",
-      location: "NSS",
-      urgency: "High",
-      dependencies: "WiFi, Tissues, Vodka"
-    },
-    {
-      id: 2,
-      task: "Take a Nap",
-      isCompleted: false,
-      dueDate: "12/5/15",
-      assignedTo: "Zoe",
-      location: "Her House",
-      urgency: "High",
-      dependencies: "Cat, Vodka"
-    }
-];
+  $scope.items = [];
 
   $scope.addNewItem = function() {
     $scope.newTask.isCompleted = false;
