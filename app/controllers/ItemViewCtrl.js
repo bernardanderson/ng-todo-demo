b6b19616df4bace151455390a9f660fb18277ce4
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-23 20:56:54 -0500

    Added filtering and single item display A single item can be selected from the object list and all of it's properties displayed

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -1,6 +1,26 @@
 "use strict";
 
-app.controller("ItemViewCtrl", function($scope){
-  $scope.welcome = "Hello";
+app.controller("ItemViewCtrl", function($scope, $http, $routeParams){
+
+  $scope.items = [];
+
+  $scope.selectedItem = {};
+
+  console.log($routeParams.itemId);
+
+  $http.get("https://ba-todo.firebaseio.com/items/.json")
+  .success(function(itemCollection){
+    Object.keys(itemCollection).forEach(function(key){
+      itemCollection[key].id = key;
+      $scope.items.push(itemCollection[key]);
+
+      $scope.selectedItem = $scope.items.filter(function(item){
+        return item.id === $routeParams.itemId;
+      })[0];
+
+
+    });
+      console.log($scope.selectedItem);
+  });
 
 });
