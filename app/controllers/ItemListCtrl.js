92e49cd605104fa542363771e3d19478c67653f7
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-23 19:35:29 -0500

    Added delete/newItem functionality

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -3,12 +3,25 @@
 app.controller("ItemListCtrl", function($scope, $http){
   $scope.items = [];
 
-  $http.get("https://ba-todo.firebaseio.com/items/.json")
-  .success(function(itemCollection){
-    Object.keys(itemCollection).forEach(function(key){
-      itemCollection[key].id = key;
-      $scope.items.push(itemCollection[key]);
+  var getItems = function() {
+    $http.get("https://ba-todo.firebaseio.com/items/.json")
+    .success(function(itemCollection){
+      Object.keys(itemCollection).forEach(function(key){
+        itemCollection[key].id = key;
+        $scope.items.push(itemCollection[key]);
+      });
     });
-  });
+  }
+
+  getItems();
+
+  $scope.itemDelete = function(itemId) {
+    $http.delete(`https://ba-todo.firebaseio.com/items/${itemId}.json`)
+    .success(function(response) {
+      console.log(response);
+      $scope.items = [];
+      getItems();
+    });
+  }
 
 });
