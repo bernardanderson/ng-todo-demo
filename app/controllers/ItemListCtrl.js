34678c01f13a74e78e3b2fce206267189ca3df7c
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-23 19:18:33 -0500

    Added Firebase access and $location forwarding

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,5 +1,14 @@
 "use strict";
 
-app.controller("ItemListCtrl", function($scope){
+app.controller("ItemListCtrl", function($scope, $http){
   $scope.items = [];
+
+  $http.get("https://ba-todo.firebaseio.com/items/.json")
+  .success(function(itemCollection){
+    Object.keys(itemCollection).forEach(function(key){
+      itemCollection[key].id = key;
+      $scope.items.push(itemCollection[key]);
+    });
+  });
+
 });
--- app/controllers/ItemNewCtrl.js
+++ app/controllers/ItemNewCtrl.js
@@ -1,16 +1,41 @@
 "use strict";
 
-app.controller("ItemNewCtrl", function($scope){
-  $scope.newTask = {};
+app.controller("ItemNewCtrl", function($scope, $http, $location){
+  $scope.newTask = {
+    assignedTo: "",
+    dependencies: "",
+    dueDate: "",
+    isCompleted: false,
+    location: "",
+    task: "",
+    urgency: ""
+  };
 
   $scope.items = [];
 
   $scope.addNewItem = function() {
-    $scope.newTask.isCompleted = false;
-    $scope.newTask.id = $scope.items.length;
-    console.log("You added a new item", $scope.newTask);
-    $scope.items.push($scope.newTask);
-    console.log("You added a new item", $scope.items);
-    $scope.newTask = "";
+
+    $http.post(
+      "https://ba-todo.firebaseio.com/items.json", // url
+      JSON.stringify({ // data
+        assignedTo: $scope.newTask.assignedTo,
+        dependencies: $scope.newTask.dependencies,
+        dueDate: $scope.newTask.dueDate,
+        isCompleted: $scope.newTask.isCompleted,
+        location: $scope.newTask.location,
+        task: $scope.newTask.task,
+        urgency: $scope.newTask.urgency
+      })
+      ).success(function(response) {
+        $location.url("/items/list");
+        console.log("The response is: ", response);
+      })
+
+    // $scope.newTask.isCompleted = false;
+    // $scope.newTask.id = $scope.items.length;
+    // console.log("You added a new item", $scope.newTask);
+    // $scope.items.push($scope.newTask);
+    // console.log("You added a new item", $scope.items);
+    // $scope.newTask = "";
   };
 });
