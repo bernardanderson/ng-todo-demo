c02d305f05973c81b0b374649dd816da7ff10598
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-25 19:22:14 -0500

    Made NavBar static and added as a partial

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,15 +1,5 @@
 "use strict";
 
 app.controller("NavCtrl", function($scope){
-  $scope.navItems = [
-  {
-    name: "All Items",
-    url: "#/items/list"
-  }, {
-    name: "New Items",
-    url: "#/items/new"
-  },{
-    name: "Logout",
-    url: "#/logout"
-  }];
+
 });
